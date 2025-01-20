1c666b52a8297ef01f9a47a4c5840bb5be503950
Sahil Sojitra <sahilsojitra.it22@scet.ac.in>
2025-01-20 09:07:16 +0000

    final code

--- src/App.js
+++ src/App.js
@@ -2,29 +2,37 @@ import logo from './logo.svg';
 import './App.css';
 import Counter from './counter';
 import Areas from './areas';
+import UserProfile from './UserProfile';
 
 
 function App() {
+  const details = {
+    name : "Sahil Sojitra",
+    age : 19,
+    location : "Surat",
+    bio : "Coder of the Future"
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 
-        </a>
-        <Counter />
-        <Areas />
-        <br></br>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React 
+    //     </a>
+    //     <Counter />
+    //     <Areas />
+    //     <br></br>
+    //   </header>
+    // </div>
+        <UserProfile  name={details.name} age={details.age} loc={details.location} bio={details.bio}/>
   );
 }
 
